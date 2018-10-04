b2ed6e030104be36c042cd8f22b8c33f5aaddce9
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-04 14:07:47 +0100

    add #view_note which take a title arg and returns the note title & body

--- lib/notes.rb
+++ lib/notes.rb
@@ -12,9 +12,14 @@ class Notes
   end
 
   def list_notes
-
     @notes.map do |note|
       note.title
     end.join("\n")
+  end
+
+  def view_note(title)
+    @notes.map do |note|
+      note.title + ": " + note.body if note.title == title
+    end.compact.join
   end
 end
--- spec/notes_spec.rb
+++ spec/notes_spec.rb
@@ -26,6 +26,15 @@ describe Notes do
       subject.add(note2)
       expect(subject.list_notes).to eq "Note 1\nNote 2"
     end
+  end
 
+  describe "#view_note"do
+    it "should return the title and body of note2" do
+      note1 = Note.new("Note 1", "This is a new note 1")
+      subject.add(note1)
+      note2 = Note.new("Note 2", "This is a new note 2")
+      subject.add(note2)
+      expect(subject.view_note("Note 2")).to eq "Note 2: This is a new note 2"
+    end
   end
 end

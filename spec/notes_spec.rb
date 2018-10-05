743d58a7ad9323759aba371a5a197517e7547735
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-05 12:56:19 +0100

    replace instances of Note with a double

--- spec/notes_spec.rb
+++ spec/notes_spec.rb
@@ -1,39 +1,47 @@
 require "notes"
 
 describe Notes do
+  let(:note) { double(:note) }
+  let(:note_1) { double(:note_1) }
+  let(:note_2) { double(:note_2) }
+
   describe "#add" do
     it "should be working" do
       expect(subject).to respond_to(:add).with(1).argument
     end
 
+    # This is a bad test
     it "notes should have a count of 1" do
-      subject.add(Note.new("New note"))
+      allow(note).to receive(:title).and_return("New note")
+      subject.add(note)
       expect(subject.notes.count).to eq 1
     end
   end
 
   describe "#list_notes" do
     it "should show 1 note with title: 'Note' body: 'This is a new note'" do
-      note = Note.new("Note", "This is a new note")
+      allow(note).to receive(:title).and_return("Note")
       subject.add(note)
       expect(subject.list_notes).to eq "Note"
     end
 
     it "should show 2 notes" do
-      note1 = Note.new("Note 1", "This is a new note 1")
-      subject.add(note1)
-      note2 = Note.new("Note 2", "This is a new note 2")
-      subject.add(note2)
+      allow(note_1).to receive(:title).and_return("Note 1")
+      allow(note_2).to receive(:title).and_return("Note 2")
+      subject.add(note_1)
+      subject.add(note_2)
       expect(subject.list_notes).to eq "Note 1\nNote 2"
     end
   end
 
   describe "#view_note"do
     it "should return the title and body of note2" do
-      note1 = Note.new("Note 1", "This is a new note 1")
-      subject.add(note1)
-      note2 = Note.new("Note 2", "This is a new note 2")
-      subject.add(note2)
+      allow(note_1).to receive(:title).and_return("Note 1")
+      allow(note_1).to receive(:body).and_return("This is a new note 1")
+      allow(note_2).to receive(:title).and_return("Note 2")
+      allow(note_2).to receive(:body).and_return("This is a new note 2")
+      subject.add(note_1)
+      subject.add(note_2)
       expect(subject.view_note("Note 2")).to eq "Note 2: This is a new note 2"
     end
   end
